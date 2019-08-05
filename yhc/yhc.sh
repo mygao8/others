3e5f723d5bd71ddd3ab6500431da43e1301ea4af
mygao8 <htz000114@gmail.com>
2019-08-05 16:45:38 +0800

    update

---
+++ yhc/yhc.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+python train_36k_lux1.py 
+python train_36k_lux2.py 
+python train_36k_lux3.py 
+python train_36k_lux4.py 
+python train_36k_lux5.py 
+python train_36k_lux6.py 
+python train_36k_lux7.py 
