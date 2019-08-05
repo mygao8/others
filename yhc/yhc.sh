8fcb8f7dbbd946aaeef10269d4517b528e89a1ab
mygao8 <htz000114@gmail.com>
2019-08-06 02:42:09 +0800

    update models to 40_lux3

--- yhc/yhc.sh
+++ yhc/yhc.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-python train_38k_lux1.py
-python train_38k_lux2.py
-python train_38k_lux3.py
-python train_38k_lux4.py
-python train_38k_lux5.py
+python train_38k_lux6.py
+python train_40k_lux1.py
+python train_40k_lux2.py
+python train_40k_lux3.py
